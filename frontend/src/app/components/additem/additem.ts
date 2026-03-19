b9805d94b99f55137a226432fa5025101eff3576
Valerii Navalnyi <valeranavalni@gmail.com>
2026-03-19 22:02:51 +0000

    Remove template from html. Create: - new components home, additem, listitems, about - service - interface

---
+++ frontend/src/app/components/additem/additem.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-additem',
+  imports: [],
+  templateUrl: './additem.html',
+  styleUrl: './additem.css',
+})
+export class Additem {
+
+}
---
+++ frontend/src/app/components/listitems/listitems.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listitems',
+  imports: [],
+  templateUrl: './listitems.html',
+  styleUrl: './listitems.css',
+})
+export class Listitems {
+
+}

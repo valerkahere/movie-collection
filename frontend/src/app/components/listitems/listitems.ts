482236cd3d2a485657e206f1762d862698dad773
Valerii Navalnyi <valeranavalni@gmail.com>
2026-03-22 19:46:25 +0000

    deleteMovie()

--- frontend/src/app/components/listitems/listitems.ts
+++ frontend/src/app/components/listitems/listitems.ts
@@ -1,10 +1,12 @@
 import { Component, inject, signal } from '@angular/core';
 import { Itemsapi } from '../../services/itemsapi/itemsapi';
 import { Movie } from '../../models/movie.interface';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-listitems',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './listitems.html',
   styleUrl: './listitems.css',
 })
@@ -13,5 +15,15 @@ export class Listitems {
     
     constructor() {
         this.movieService.getItems();
+    }
+
+    id = signal<string>("");
+    onSubmit() {
+        // 1. Send data to service
+        this.movieService.deleteItem(
+            this.id()
+        );  
+        // 2. Clear the form signals
+        this.id.set("");
     }
 }
